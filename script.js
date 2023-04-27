abc7532312352d6916f14f712fa5418c31ca832f
Alexis Goldberg <alexisaeg12@gmail.com>
2023-04-27 00:16:29 -0400

    features, moving code for better response, filter

--- script.js
+++ script.js
@@ -1,82 +1,3 @@
-
-let myMap = null;
-
-function initializeMap() {
-  if (myMap) {
-    return myMap;
-  }
-
-  let mapElement = document.getElementById("map");
-
-  if (!mapElement) {
-    mapElement = document.createElement("div");
-    mapElement.id = "map";
-    document.body.appendChild(mapElement);
-  }
-
-  myMap = L.map(mapElement,{
-    attributionControl:false,
-  });
-
-
-  const tectonicPlatesUrl =
-    "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json";
-
-  fetch(tectonicPlatesUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      const tectonicPlatesLayer = L.geoJSON(data, {
-        style: function (feature) {
-          return {
-            color: "#ff0000",
-            weight: 3,
-          };
-        },
-      });
-
-      const bounds = tectonicPlatesLayer.getBounds();
-      const maxBounds = bounds.pad(0);
-      myMap.setMaxBounds(maxBounds);
-      myMap.fitBounds(bounds).setZoom(1.45);
-      tectonicPlatesLayer.addTo(myMap);
-
-      const tectonicPlatesAttribution =
-        'Tectonic Plate Boundaries &copy; <a href="https://github.com/fraxen/tectonicplates">Fraxen</a>';
-      if (myMap.attributionControl) {
-        myMap.attributionControl.addAttribution(tectonicPlatesAttribution);
-      }
-    })
-    .catch((error) => {});
-
-  const baseLayer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-  });
-  baseLayer.addTo(myMap);
-  const earthquakeUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-01-02";
-  fetch(earthquakeUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      const markers = L.markerClusterGroup();
-      data.features.forEach((feature) => {
-        const coords = feature.geometry.coordinates.slice(0, 2).reverse(); 
-        const magnitude = feature.properties.mag;
-        const popupText = `<strong>Magnitude:</strong> ${magnitude} &nbsp; `
-        const marker = L.marker(coords).bindPopup(popupText);
-        markers.addLayer(marker);
-      });
-      markers.addTo(myMap);
-    })
-    .catch((error) => {});
-    
-  if (typeof L.grid === 'function') {
-    L.grid().remove();
-  }
-  
-  return myMap;
-}
-if (document.getElementsByClassName("map-container").length > 0) {
-  initializeMap();
-}
 window.addEventListener("scroll", function () {
   const banner = document.getElementById("banner");
   const line = document.getElementById("line");
@@ -93,3 +14,118 @@ window.addEventListener("scroll", function () {
     line.style.width = lineW + "px";
   }
 });
+let myMap = null;
+let earthquakeMarkers = L.layerGroup();
+let markers = L.markerClusterGroup();
+
+function initializeMap() {
+
+  if (typeof L === "undefined") {
+    return;
+  }
+  if (myMap) {
+    return myMap;
+  }
+
+  let mapElement = document.getElementById("map");
+  
+
+  if (!mapElement) {
+    mapElement = document.createElement("div");
+    mapElement.id = "map";
+    document.body.appendChild(mapElement);
+  }
+
+  myMap = L.map(mapElement, {
+    attributionControl: false,
+    preferCanvas:true,
+  });
+  const tectonicPlatesLayerGroup = L.layerGroup();
+  const tectonicPlatesUrl =
+    "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json";
+
+  fetch(tectonicPlatesUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const tectonicPlatesLayer = L.geoJSON(data, {
+        style: function (feature) {
+          return {
+            color: "#ff0000",
+            weight: 3,
+          };
+        },
+      });
+      const bounds = tectonicPlatesLayer.getBounds();
+      const maxBounds = bounds.pad(0);
+      myMap.setMaxBounds(maxBounds);
+      myMap.fitBounds(bounds).setZoom(1.45);
+      tectonicPlatesLayerGroup.addLayer(tectonicPlatesLayer);
+      tectonicPlatesLayerGroup.addTo(myMap);
+
+      const tectonicPlatesAttribution =
+        'Tectonic Plate Boundaries &copy; <a href="https://github.com/fraxen/tectonicplates">Fraxen</a>';
+      if (myMap.attributionControl) {
+        myMap.attributionControl.addAttribution(tectonicPlatesAttribution);
+      }
+    })
+  const baseLayer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+  });
+  baseLayer.addTo(myMap);
+  const initialMarkers = L.layerGroup();
+  initialMarkers.addTo(myMap);
+  const earthquakeUrl ="https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2023-04-01&endtime=2023-04-04";
+  fetch(earthquakeUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      data.features.forEach((feature) => {
+        const coords = feature.geometry.coordinates.slice(0, 2).reverse();
+        const magnitude = feature.properties.mag;
+        const popupText = `<strong>Magnitude:</strong> ${magnitude} &nbsp; `;
+        const marker = L.marker(coords).bindPopup(popupText);
+        marker.magnitude = magnitude;
+        earthquakeMarkers.addLayer(marker);
+      });
+      earthquakeMarkers.addTo(myMap);
+    });
+  const magnitudeFilter = document.getElementById("magnitude-filter");
+  magnitudeFilter.addEventListener("change", function () {
+    filterMarkers(magnitudeFilter.value, earthquakeMarkers);
+  });
+
+  if (typeof L.grid === 'function') {
+    L.grid();
+  }
+
+  return myMap;
+}
+function filterMarkers(range, markers) {
+  clearMarkers(markers);
+
+  markers.eachLayer(function (marker) {
+    const magnitude = marker.magnitude;
+    if (
+      range === "" ||
+      (magnitude >= parseFloat(range) && magnitude < parseFloat(range) + 1)
+    ) {
+      marker.addTo(myMap);
+    }
+  });
+}
+
+
+
+function clearMarkers(markers) {
+  markers.eachLayer(function (marker) {
+    marker.removeFrom(myMap);
+  });
+}
+
+const magnitudeFilter = document.getElementById("magnitude-filter");
+magnitudeFilter.addEventListener("change", function () {
+  clearMarkers(markers);
+  filterMarkers(magnitudeFilter.value, markers);
+});
+
+initializeMap();
+
