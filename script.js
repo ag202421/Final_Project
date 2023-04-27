967d23051eb9eea8f2ec286e7c075b3ef6b3806f
Alexis Goldberg <alexisaeg12@gmail.com>
2023-04-27 01:51:43 -0400

    optimize scroll

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-window.addEventListener("scroll", function () {
-  const banner = document.getElementById("banner");
-  const line = document.getElementById("line");
-  const bannerHt = banner.offsetHeight;
-  const pageHt = document.body.offsetHeight;
-  const scrollPos = window.scrollY;
-  const scrollPer = scrollPos / (pageHt - bannerHt);
-  const lineW = scrollPer * banner.offsetWidth;
-  const maxScroll = pageHt - window.innerHeight;
-  line.style.width = lineW + "px";
-  if (scrollPos >= maxScroll) {
-    line.style.width = banner.offsetWidth + "px";
-  } else {
-    line.style.width = lineW + "px";
-  }
-});
 let myMap = null;
 let earthquakeMarkers = L.layerGroup();
 let markers = L.markerClusterGroup();
@@ -128,4 +112,3 @@ magnitudeFilter.addEventListener("change", function () {
 });
 
 initializeMap();
-
